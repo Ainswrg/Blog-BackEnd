00bea8226e647ec5b7c4605b7b7a83e48ac419c0
Jaslan <Jaslan.dlnv@gmail.com>
2022-08-23 10:09:24 +0500

    feat: add cors && getLastTags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,21 @@
 import PostModel from "../models/Post.js";
 
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    res.status(500).json({
+      message: "Can't find any tags",
+    });
+  }
+};
 export const getAll = async (req, res) => {
   try {
     const posts = await PostModel.find().populate("user").exec();
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import multer from "multer";
+import cors from "cors";
 
 import mongoose from "mongoose";
-import multer from "multer";
 
 import {
   registerValidation,
@@ -33,6 +34,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
@@ -59,7 +61,10 @@ app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   });
 });
 
+app.get("/tags", PostController.getLastTags);
+
 app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
 app.get("/posts/:id", PostController.getOne);
 app.post(
   "/posts",
